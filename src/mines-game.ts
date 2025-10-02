3fcb73ca2905c0de5e25cf67605e70efc2fa014a
Emanuel Rico Rojas <37458534+Drd3@users.noreply.github.com>
2025-10-02 11:56:33 -0500

    creating mines game subgraph

--- src/mines-game.ts
+++ src/mines-game.ts
@@ -1,11 +1,30 @@
-import { GameCreated } from "../generated/MinesGame/MinesGame";
+import { GameCreated, CellResolved, CommitCell } from "../generated/MinesGame/MinesGame";
+import { CashedOut } from "../generated/MinesGame/MinesGame";
 import { MineGame } from "../generated/schema";
-import { BigInt } from "@graphprotocol/graph-ts";
+import { BigInt, Bytes } from "@graphprotocol/graph-ts";
+
+// get-or-create helper for MineGame
+function getOrCreateMineGame(id: Bytes): MineGame {
+  let entity = MineGame.load(id);
+  if (entity == null) {
+    entity = new MineGame(id);
+    // minimal defaults; specific fields will be set by the handler
+    entity.cumMul = BigInt.zero();
+    entity.steps = 0;
+    entity.active = false;
+    entity.vrfPending = false;
+    entity.vrfRequestId = BigInt.zero();
+    entity.isMine = false;
+    entity.revealedCells = [];
+    entity.cashedOut = false;
+    entity.cashoutAmount = BigInt.zero();
+  }
+  return entity as MineGame;
+}
 
 export function handleGameCreated(event: GameCreated): void {
-  let entity = new MineGame(
-    event.transaction.hash.concatI32(event.params.player.toI32())
-  );
+  let id = event.transaction.hash.concatI32(event.params.player.toI32());
+  let entity = getOrCreateMineGame(id);
 
   entity.player = event.params.player;
   entity.gameId = event.params.gameId;
@@ -30,15 +49,50 @@ export function handleGameCreated(event: GameCreated): void {
   entity.isMine = false;
   entity.revealedCells = [];
   entity.cashedOut = false;
-  entity.cashoutAmount = BigInt.fromI32(0);
 
   entity.save();
 }
 
-export function handleCellResolved(event: CellResolved): void {
+export function handleCommitCell(event: CommitCell): void {
+  // TODO: Implement once ID strategy is finalized (likely player + gameId)
+  let id = event.transaction.hash.concatI32(event.params.player.toI32());
+  let entity = getOrCreateMineGame(id);
 
-  // Generate the get or create
-
-   entity.cells.push(event.params.cellId.toI32());
-   entity.save()
+  entity.vrfPending = true;
+  entity.revealedCells.push(event.params.cellIndex.toI32());
+  entity.save();
 }
+
+export function handleCellResolved(event: CellResolved): void {
+  // TODO: Implement once ID strategy is finalized (likely player + gameId)
+  let id = event.transaction.hash.concatI32(event.params.player.toI32());
+  let entity = getOrCreateMineGame(id);
+
+  entity.isMine = event.params.isMine;
+  entity.vrfPending = false;
+  entity.vrfRequestId = BigInt.zero();
+  entity.revealedCells.push(event.params.cellIndex.toI32());
+  entity.save();
+}
+
+export function handleCashedOut(event: CashedOut): void {
+  // TODO: Implement once ID strategy is finalized (likely player + gameId)
+  let id = event.transaction.hash.concatI32(event.params.player.toI32());
+  let entity = getOrCreateMineGame(id);
+
+  entity.cashedOut = true;
+  entity.cashoutAmount = event.params.amount;
+  entity.active = false;
+  // Reset gameplay-related state
+  entity.vrfPending = false;
+  entity.vrfRequestId = BigInt.zero();
+  entity.isMine = false;
+  entity.revealedCells = [];
+  entity.steps = 0;
+  entity.cumMul = BigInt.zero();
+  entity.remainingCells = 0;
+  entity.remainingMines = 0;
+  entity.stake = BigInt.zero();
+  entity.save();
+}
+
